e5c6cf8d45deaecabfb471edfe1281ea12ec6e17
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2022-08-01 17:10:04 +0600

    One type of food has been added to increase life of the snake

--- snake.js
+++ snake.js
@@ -6,12 +6,20 @@ let lifeSpan;
 let headImage;
 let appleImage;
 let bodyImage;
+let lifeImage;
 let boosterAppleImage;
 
 let apple = {
     x: 0,
     y: 0,
 };
+
+let life = {
+    x: -40,
+    y: -40,
+    locateLife: false,
+    drawLife: false,
+}
 
 let boosterApple = {
     x: -50,
@@ -47,6 +55,8 @@ const DOWN_KEY = 40;
 
 let score = 0;
 let count = 0;
+let lifeTimeCount = 0;
+let showLifeAfterAppleEaten = 1;
 
 
 function init() {
@@ -73,6 +83,9 @@ function loadImages() {
     appleImage = new Image();
     appleImage.src = 'images/apple.png'; 
 
+    lifeImage = new Image();
+    lifeImage.src = 'images/life.jpg';
+
     boosterAppleImage = new Image();
     boosterAppleImage.src = "images/booster_apple.png";
 }
@@ -97,6 +110,14 @@ function doDrawing() {
                 setTimeout('dissappearBoosterApple()', 5000);
             }
             drawBoosterApple();
+        }
+
+        if(lifeTimeCount >= showLifeAfterAppleEaten){
+            locateLife();
+            if(!life.drawLife && lifeTimeCount == showLifeAfterAppleEaten) {
+                setTimeout("dissappearLife()", 2500);
+            }
+            drawLife();
         }
 
         canvasContext.drawImage(appleImage, apple.x, apple.y);
@@ -131,6 +152,7 @@ function checkApple() {
 
         score++;
         count++;
+        lifeTimeCount++;
         scoreSpan.innerText = score;
         locateApple();
     }
@@ -157,6 +179,10 @@ function dissappearBoosterApple() {
         } else {
             canvasContext.drawImage(bodyImage, snake.x[z], snake.y[z]);
         }
+    }
+
+    if(life.drawLife) {
+        drawLife();
     }
     
     boosterApple.locateApple = false;
@@ -234,6 +260,7 @@ function gameCycle() {
 
         checkApple();
         checkBoosterApple();
+        checkLife();
         checkCollision();
         move();
         doDrawing();
@@ -282,4 +309,54 @@ function resetAfterGetKilled() {
     } else {
         createSnake();
     }
+}
+
+function locateLife() {
+
+    if(!life.locateLife && lifeTimeCount == showLifeAfterAppleEaten) {
+        do {
+            life.x = Math.floor(Math.random() * MAX_RAND) * CELL_SIZE;
+            life.y = Math.floor(Math.random() * MAX_RAND) * CELL_SIZE;
+        } while((life.x == apple.x) && (life.y == apple.y) || (life.x == boosterApple.x) && (life.y == boosterApple.y));
+        
+        life.locateLife = true;
+    }
+}
+
+function drawLife() {
+    
+    canvasContext.drawImage(lifeImage, life.x, life.y);
+    life.drawLife = true;
+}
+
+function checkLife() {
+    if ((snake.x[0] == life.x) && (snake.y[0] == life.y)) {
+        snake.life += 4;
+        lifeSpan.innerText = snake.life;
+        dissappearLife();
+    }
+}
+
+function dissappearLife() {
+    lifeTimeCount = 0;
+
+    canvasContext.clearRect(0, 0, CANVAS_HEIGHT, CANVAS_WIDTH);
+    canvasContext.drawImage(appleImage, apple.x, apple.y);
+
+    for (let z = 0; z < snake.size; z++) {
+        
+        if (z == 0) {
+            canvasContext.drawImage(headImage, snake.x[z], snake.y[z]);
+        } else {
+            canvasContext.drawImage(bodyImage, snake.x[z], snake.y[z]);
+        }
+    }
+
+    if(boosterApple.drawBoosterApple) {
+        drawBoosterApple();
+    }
+
+    showLifeAfterAppleEaten = Math.floor(Math.random() * MAX_RAND);
+    life.locateLife = false;
+    life.drawLife = false;
 }
