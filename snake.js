a09d00b09655e5008541e54fc60db95bb92b704c
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2022-08-01 10:54:13 +0600

    Booster apple feature has been added

--- snake.js
+++ snake.js
@@ -5,10 +5,17 @@ let scoreSpan;
 let headImage;
 let appleImage;
 let bodyImage;
+let boosterAppleImage;
 
 let apple = {
     x: 0,
     y: 0,
+};
+
+let boosterApple = {
+    x: 200,
+    y: 200,
+    showBoosterApple: true
 };
 
 let snake = {
@@ -28,6 +35,7 @@ const MAX_RAND = 29;
 const CELL_SIZE = 10;
 const CANVAS_WIDTH = 300; 
 const CANVAS_HEIGHT = 300;
+const SHOW_BOOSTERAPPLE_AFTER_APPLE_EATEN = 5;
    
 const LEFT_KEY = 37;
 const RIGHT_KEY = 39;
@@ -35,6 +43,7 @@ const UP_KEY = 38;
 const DOWN_KEY = 40;
 
 let score = 0;
+let count = 0;
 
 
 function init() {
@@ -59,6 +68,9 @@ function loadImages() {
     
     appleImage = new Image();
     appleImage.src = 'images/apple.png'; 
+
+    boosterAppleImage = new Image();
+    boosterAppleImage.src = "images/booster_apple.png";
 }
 
 function createSnake() {
@@ -74,6 +86,11 @@ function doDrawing() {
     canvasContext.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     
     if (inGame) {
+
+        if(count >= SHOW_BOOSTERAPPLE_AFTER_APPLE_EATEN) {
+            drawBoosterApple();
+            setTimeout('dissappearBoosterApple()', 5000);
+        }
 
         canvasContext.drawImage(appleImage, apple.x, apple.y);
 
@@ -106,9 +123,37 @@ function checkApple() {
     if ((snake.x[0] == apple.x) && (snake.y[0] == apple.y)) {
 
         score++;
+        count++;
         scoreSpan.innerText = score;
         locateApple();
     }
+}
+
+function checkBoosterApple() {
+    if ((snake.x[0] == boosterApple.x) && (snake.y[0] == boosterApple.y)) {
+        score += 3;
+        scoreSpan.innerText = score;
+        dissappearBoosterApple();
+    }
+}
+
+function dissappearBoosterApple() {
+    count = 0;
+    boosterApple.x = -30;
+    boosterApple.y = -50;
+
+    canvasContext.clearRect(0, 0, CANVAS_HEIGHT, CANVAS_WIDTH);
+    canvasContext.drawImage(appleImage, apple.x, apple.y);
+
+    for (let z = 0; z < snake.size; z++) {
+        
+        if (z == 0) {
+            canvasContext.drawImage(headImage, snake.x[z], snake.y[z]);
+        } else {
+            canvasContext.drawImage(bodyImage, snake.x[z], snake.y[z]);
+        }
+    }    
+    locateBoosterApple();
 }
 
 function move() {
@@ -158,13 +203,24 @@ function locateApple() {
 
     apple.x = Math.floor(Math.random() * MAX_RAND) * CELL_SIZE;
     apple.y = Math.floor(Math.random() * MAX_RAND) * CELL_SIZE;
-}    
+}
+
+function locateBoosterApple() {
+
+    boosterApple.x = Math.floor(Math.random() * MAX_RAND) * CELL_SIZE;
+    boosterApple.y = Math.floor(Math.random() * MAX_RAND) * CELL_SIZE;
+}
+
+function drawBoosterApple() {
+    canvasContext.drawImage(boosterAppleImage, boosterApple.x, boosterApple.y);
+}
 
 function gameCycle() {
     
     if (inGame) {
 
         checkApple();
+        checkBoosterApple();
         checkCollision();
         move();
         doDrawing();
